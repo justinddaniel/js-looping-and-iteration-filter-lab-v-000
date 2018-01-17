c0d542d6a23a6b7f76bc293b557d3fb53e139c45
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 02:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching (drivers, string) {
-  let newdrivers = drivers.filter(function (name) {return name === string; });
+  let newdrivers = drivers.filter(function (name) {return name.toUpperCase() === string.toUpperCase(); });
   return newdrivers;
 }
